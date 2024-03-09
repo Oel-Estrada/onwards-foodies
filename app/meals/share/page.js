5c9b63f8f7f25972c964a1863e424be82ea05cf2
Oel Estrada <oelestradacampos@gmail,com>
2024-03-09 00:02:58 -0500

    feat: add documentation to SharePage component
    A comment block has been added to the MealsPage component to accurately describe its purpose and return value. This ensures better code comprehension and maintenance for future reference.

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,3 +1,8 @@
+/**
+ * Represents the Share Page component for displaying the meals share page.
+ *
+ * @returns {ReactElement}.
+ */
 const SharePage = () => <h1>Meals Share Page</h1>;
 
 export default SharePage;
