bd43d356c5419fc2337cdb79fc9605c98173fd24
Oel Estrada <oelestradacampos@gmail,com>
2024-03-10 02:06:09 -0400

    feat: update Home component layout and import new CSS module
    The Home component's layout in `page.js` was reorganized for improved appearance and readability. This restructuring also required the addition of a new CSS module, `page.module.css`, to manage styles more efficiently. This new module provides style rules for various elements, including header, slideshow, hero, and call-to-action components.

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import classes from "./page.module.css";
 import Link from "next/link";
 
 /**
@@ -6,20 +7,22 @@ import Link from "next/link";
  * @returns {JSX.Element} The rendered Home component.
  */
 const Home = () => (
-  <main>
-    <h1 style={{ color: "white", textAlign: "center" }}>
-      Time to get started!
-    </h1>
-    <p>
-      <Link href="/meals">Meals</Link>
-    </p>
-    <p>
-      <Link href="/meals/share">Share Meal</Link>
-    </p>
-    <p>
-      <Link href="/community">Community</Link>
-    </p>
-  </main>
+  <>
+    <header className={classes.header}>
+      <div className={classes.slideshow}></div>
+      <div>
+        <div className={classes.hero}>
+          <h1>NextLevel Food for NextLevel Foodies</h1>
+          <p>Taste & share food from all over the world.</p>
+        </div>
+        <div className={classes.cta}>
+          <Link href="/community">Join the Community</Link>
+          <Link href="/meals">Explore Meals</Link>
+        </div>
+      </div>
+    </header>
+    <main></main>
+  </>
 );
 
 export default Home;
