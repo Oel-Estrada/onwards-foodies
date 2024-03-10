7d2c8f79ee020e0e698f85830030b4cd465757bb
Oel Estrada <oelestradacampos@gmail,com>
2024-03-10 01:43:59 -0400

    feat: introduced MainHeaderBackground component for cleaner code
    Shifted header background rendering into its own component, MainHeaderBackground, separating the SVG and its styling from layout.js. This cleans up the main layout file and supports modularity, allowing for easier adjustment and testing of different header designs.

--- app/main-header.js
+++ app/main-header.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
+import MainHeaderBackground from "@/app/main-header-background";
 
 /**
  * Represents the links displayed in the navigation bar.
@@ -21,21 +22,24 @@ const navbarLinks = [
  * @returns {JSX.Element}
  */
 const MainHeader = () => (
-  <header className={classes.header}>
-    <Link className={classes.logo} href="/">
-      <Image src={logoImg} alt="A plate with food on it" priority />
-      NextLevel Food
-    </Link>
-    <nav className={classes.nav}>
-      <ul>
-        {navbarLinks.map(({ label, route }) => (
-          <li key={route}>
-            <Link href={route}>{label}</Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  </header>
+  <>
+    <MainHeaderBackground />
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
+        <Image src={logoImg} alt="A plate with food on it" priority />
+        NextLevel Food
+      </Link>
+      <nav className={classes.nav}>
+        <ul>
+          {navbarLinks.map(({ label, route }) => (
+            <li key={route}>
+              <Link href={route}>{label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  </>
 );
 
 export default MainHeader;
