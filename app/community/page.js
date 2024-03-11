88c8b200ec0a36551fcf76983a0673335fe7c032
Oel Estrada <oelestradacampos@gmail,com>
2024-03-10 23:30:12 -0400

    feat: add community page styling and structure
    The commit adds new features to the community page by including a CSS module for styling. This also involves adjusting the structure of the page to display multiple PerkItems. The new file includes specific styles for elements such as headers, main content, and perks, enhancing the overall page's look and feel.

--- app/community/page.js
+++ app/community/page.js
@@ -1,8 +1,51 @@
+import mealIcon from "@/assets/icons/meal.png";
+import communityIcon from "@/assets/icons/community.png";
+import eventsIcon from "@/assets/icons/events.png";
+import classes from "./page.module.css";
+import PerkItem from "@/components/PerkItem/PerkItem";
+
+/**
+ * Define perk items information
+ */
+const perkItems = [
+  { icon: mealIcon, alt: "A delicious meal", text: "Share & discover recipes" },
+  {
+    icon: communityIcon,
+    alt: "A crowd of people, cooking",
+    text: "Find new friends & like-minded people",
+  },
+  {
+    icon: eventsIcon,
+    alt: "A crowd of people at a cooking event",
+    text: "Participate in exclusive events",
+  },
+];
+
 /**
  * Represents the community page component.
  *
  * @returns {JSX.Element}
  */
-const CommunityPage = () => <h1>Community Page</h1>;
+const CommunityPage = () => (
+  <>
+    <header className={classes.header}>
+      <h1>
+        One shared passion: <span className={classes.highlight}>Food</span>
+      </h1>
+      <p>Join our community and share your favorite recipes!</p>
+    </header>
+    <main className={classes.main}>
+      <h2>Community Perks</h2>
+
+      <ul className={classes.perks}>
+        {perkItems.map((perkItems, index) => (
+          <PerkItem key={index} icon={perkItems.icon} alt={perkItems.alt}>
+            {perkItems.text}
+          </PerkItem>
+        ))}
+      </ul>
+    </main>
+  </>
+);
 
 export default CommunityPage;
